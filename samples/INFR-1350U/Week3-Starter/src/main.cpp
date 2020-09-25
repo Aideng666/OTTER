ca1d44b901f48399fabba2570007a1dd3ff06fb8
Aiden <aiden.gimpel@ontariotechu.net>
2020-09-25 15:26:16 -0400

    Implemented Shader.cpp

--- samples/INFR-1350U/Week3-Starter/src/main.cpp
+++ samples/INFR-1350U/Week3-Starter/src/main.cpp
@@ -53,6 +53,12 @@ void GlDebugMessage(GLenum source, GLenum type, GLuint id, GLenum severity, GLsi
 
 GLFWwindow* window;
 
+void GlfwWindowResizedCallback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+
 bool initGLFW() {
 	if (glfwInit() == GLFW_FALSE) {
 		LOG_ERROR("Failed to initialize GLFW");
@@ -62,6 +68,9 @@ bool initGLFW() {
 	//Create a new GLFW window
 	window = glfwCreateWindow(800, 800, "INFR1350U", nullptr, nullptr);
 	glfwMakeContextCurrent(window);
+
+	glfwSetWindowSizeCallback(window, GlfwWindowResizedCallback);
+
 
 	return true;
 }
