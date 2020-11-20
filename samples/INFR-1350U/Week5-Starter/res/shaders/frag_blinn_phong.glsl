634b58da3015286572ed5744c5672297505e52df
Aiden <aiden.gimpel@ontariotechu.net>
2020-11-20 14:03:02 -0500

    idk

--- samples/INFR-1350U/Week5-Starter/res/shaders/frag_blinn_phong.glsl
+++ samples/INFR-1350U/Week5-Starter/res/shaders/frag_blinn_phong.glsl
@@ -17,6 +17,11 @@ uniform vec3  u_CamPos;
 
 out vec4 frag_color;
 
+const float lightIntensity = 10.0;
+
+const int bands = 5;
+const float scaleFactor = 1.0 / bands;
+
 // https://learnopengl.com/Advanced-Lighting/Advanced-Lighting
 void main() {
 	// Lecture 5
@@ -24,6 +29,7 @@ void main() {
 
 	// Diffuse
 	vec3 N = normalize(inNormal);
+	vec3 V = normalize(u_CamPos - inPos);
 	vec3 lightDir = normalize(u_LightPos - inPos);
 
 	float dif = max(dot(N, lightDir), 0.0);
@@ -32,13 +38,18 @@ void main() {
 	//Attenuation
 	float dist = length(u_LightPos - inPos);
 	diffuse = diffuse / dist; // (dist*dist)
+	
+	vec3 diffuseOut = (diffuse * inColor) / (dist*dist);
+    
+    diffuseOut = diffuseOut*lightIntensity;
+
+    //Lecture 10c
+    diffuseOut = floor(diffuseOut * bands) * scaleFactor;
+
+
+	float edge = (dot(V, inNormal) < 0.4) ? 0.0 : 1.0;
 
 	// Specular
-
-	// REPLACE THIS SECTION FOR BLINN-PHONG
-//	vec3 camDir = normalize(u_CamPos - inPos);
-//	vec3 reflectDir = reflect(-lightDir, N);
-//	float spec = pow(max(dot(camDir, reflectDir), 0.0), u_Shininess); // Shininess coefficient (can be a uniform)
 	vec3 viewDir = normalize(u_CamPos - inPos);
     vec3 h         = normalize(lightDir + viewDir);
     float spec = pow(max(dot(N, h), 0.0), u_Shininess); // Shininess coefficient (can be a uniform)
@@ -46,7 +57,8 @@ void main() {
 
 	vec3 specular = u_SpecularLightStrength * spec * u_LightCol; // Can also use a specular color
 
-	vec3 result = (ambient + diffuse + specular) * (1.0, 1.0, 1.0);
+	vec3 result = diffuseOut * edge;
+	//vec3 result = (ambient + diffuse + specular) * (1.0, 1.0, 1.0);
 
 	frag_color = vec4(result, 1.0);
 }
